b4f7e28fd519d935b370198d3a911a9db99c1bc2
sme12 <sazanovvs@gmail.com>
2026-04-03 16:00:38 +0300

    fix: linting errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,22 @@
-// @ts-check
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
   {
     plugins: {
       react,
